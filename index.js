386e8c8d748c77f8be44c765cd166551d2207880
Sarita200 <saritasadgir1@gmail.com>
2024-07-17 19:20:34 +0530

    connect to mongoDB

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 import { getHealth } from "./controllers/health.js";
 import { postPlant,getPlant,getPlantID,putPlantID,deletePlantID } from "./controllers/plants.js";
@@ -7,7 +8,20 @@ import {handlePageNotFound} from "./controllers/errors.js"
 
 dotenv.config()
 const app = express();
+
+const PORT = process.env.PORT ;
 app.use(express.json());
+
+(async()=>{
+    const conn = await mongoose.connect("mongodb+srv://sarita1:phphatsdfg@cluster0.07emoys.mongodb.net/Nursery-server")
+
+    if(conn){
+        console.log(`MongoDB Connected 📦`)
+    }
+    else{
+        console.log(`MongoDB Not Connected ❌`)
+    }
+}) ();
 
 
 app.get("/health",getHealth)
@@ -16,10 +30,8 @@ app.get("/plants",getPlant)
 app.get("/plant/:id",getPlantID)
 app.put("/plant/:id",putPlantID)
 app.delete("/plant/:id",deletePlantID)
-
 app.use("*",handlePageNotFound)
 
-const PORT = process.env.PORT ;
 
 app.listen(PORT, () => {
     console.log(`server is running on ${PORT}`);
