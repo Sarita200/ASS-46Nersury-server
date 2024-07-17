dff20e84d76d866624a2a03af749e6a79264762e
Sarita200 <saritasadgir1@gmail.com>
2024-07-17 22:41:51 +0530

    save data to database

--- controllers/errors.js
+++ controllers/errors.js
@@ -1,6 +1,8 @@
 const handlePageNotFound =(req,res)=>{
     res.send(`<div>
-        <h1 style="text-align:center">404 Not Found</h1>`)
+        <h1 style="text-align:center">404 Not Found</h1>
+        </div>
+        `)
 }
 
 export{
--- controllers/plants.js
+++ controllers/plants.js
@@ -1,30 +1,8 @@
-const plants = [{
-    "id":1 ,
-    "name":"bamboo",
-    "category":"indoor",
-    "image":"https://www.kokedamaindia.com/wp-content/uploads/2020/08/lucky-bamboo-new1-550x669.jpg",
-    "price":150,
-    "description":"Lucky Bamboo Plant Kokedama"
-},
-{
-    "id": 2,
-    "name": "pink rose",
-    "category": "outdoor",
-    "image": "https://m.media-amazon.com/images/I/41aTQhHbg4L._SX300_SY300_QL70_FMwebp_.jpg",
-    "price": 210,
-    "description": "Royal Paradise Garden American Pink Rose Flower Plant Home Garden Plant 1 Grafted Rose Live Plant"
-},
-{
-    "id": 3,
-    "name": "rose",
-    "category": "outdoor",
-    "image": "https://cdn2.stylecraze.com/wp-content/uploads/2013/08/1404-Top-25-Most-Beautiful-Red-Roses.jpg.webp",
-    "price": 300,
-    "description": "Top 27 Most Beautiful Red Roses"
-}
-]
+import Plant from "./../models/Plant.js"
+const plants = []
 
-const postPlant = (req, res) => {
+
+const postPlant = async (req, res) => {
     const {
         name,
         category,
@@ -33,54 +11,15 @@ const postPlant = (req, res) => {
         description 
     } = req.body
 
-        if(!name ){
-            return res.json({
-                success :false,
-                data :null,
-                message :"Name is required......"
-            })
-        }
-        if(!category){
-            return res.json({
-                success :false,
-                data :null,
-                message :"category is required......"
-            })
-        }
-        if(!image){
-            return res.json({
-                success :false,
-                data :null,
-                message :"image is required......"
-            })
-        }
-        if(!price){
-            return res.json({
-                success :false,
-                data :null,
-                message :"price is required......"
-            })
-        }
-        if(!description){
-            return res.json({
-                success :false,
-                data :null,
-                message :"description is required......"
-            })
-        }
+        const newPlant = new Plant({
+            name :name,
+            category :category,
+            image :image,
+            price :price,
+            description :description 
+        })
 
-    const randomId = Math.round(Math.random() * 10000)
-
-    const newPlant = {
-        id: randomId,
-        name: name ,
-        category: category,
-        image: image,
-        price: price,
-        description: description
-    }
-
-    plants.push(newPlant)
+        const savePlant =await newPlant.save();
 
     res.json({
         succes: true,
@@ -89,10 +28,12 @@ const postPlant = (req, res) => {
     })
 }
 
-const getPlant =(req,res) =>{
+const getPlant =async (req,res) =>{
+
+    const allPlants = await Plant.find()
     res.json({
         success :true,
-        data :plants,
+        data :allPlants,
         message :"All plants fetch successfully"
     })
 }
